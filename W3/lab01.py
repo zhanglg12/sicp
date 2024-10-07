1254b27b6b58fd5393d238089e52d847d630c734
zlg <lvgangzhang@gmail.com>
2024-10-07 14:42:35 +0800

    W3

---
+++ W3/lab01.py
@@ -0,0 +1,62 @@
+def both_odd(a, b):
+    """Returns True if both a and b are odd numbers.
+
+    >>> both_odd(-1, 1)
+    True
+    >>> both_odd(2, 1)
+    False
+    """
+    return a and b % 2 == 1 # You can replace this line!
+
+
+def factorial(n):
+    """Return the factorial of a positive integer n.
+
+    >>> factorial(3)
+    6
+    >>> factorial(5)
+    120
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def is_triangle(a, b, c):
+    """Given three integers (may be nonpositive), judge whether the three
+    integers can form the three sides of a triangle.
+
+    >>> is_triangle(2, 1, 3)
+    False
+    >>> is_triangle(5, -3, 4)
+    False
+    >>> is_triangle(2, 2, 2)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def number_of_nine(n):
+    """Return the number of 9 in each digit of a positive integer n.
+
+    >>> number_of_nine(999)
+    3
+    >>> number_of_nine(9876543)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def min_digit(x):
+    """Return the min digit of x.
+
+    >>> min_digit(10)
+    0
+    >>> min_digit(4224)
+    2
+    >>> min_digit(1234567890)
+    0
+    >>> # make sure that you are using return rather than print
+    >>> a = min_digit(123)
+    >>> a
+    1
+    """
+    "*** YOUR CODE HERE ***"
